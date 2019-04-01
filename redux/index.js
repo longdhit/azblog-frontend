4aed6bc087d00c4ccac35b68e438cdab472d37db
longdhit <longdhit@gmail.com>
2019-04-01 10:19:26 +0700

    up

---
+++ redux/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+
+export const initStore = (initialState = {}) => {
+  return createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunk)));
+};
